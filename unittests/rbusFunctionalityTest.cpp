31b959d5ab5b67c3b9d6996476bc61e8e1c33be1
Praveen <praveenirumpanangadu@gmail.com>
2024-12-10 12:37:22 +0530

    Update rbusFunctionalityTest.cpp
    Valgrind can slow down execution, potentially exposing race conditions or timing issues. The use of fork() and usleep() might be sensitive to timing changes.

--- unittests/rbusFunctionalityTest.cpp
+++ unittests/rbusFunctionalityTest.cpp
@@ -55,7 +55,7 @@ static void exec_func_test(rbusGtest_t test)
   pid_t pid = fork();
   if (0 == pid) {
     int ret = 0;
-    usleep(50000);
+    usleep(550000);
     ret = rbusConsumer(test, 0, runtime);
     exit(ret);
   } else {
